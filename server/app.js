939b3224db69c5e09675eb7860eedb904774e942
Muntaha <muntahasayed396@gmail.com>
2025-10-21 20:06:37 +0530

    Updated code structure

--- server/app.js
+++ server/app.js
@@ -2,60 +2,13 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 const http = require('http')
-const { Server } = require('socket.io')
+const { socketInit } = require('./sockets/index');
 
 app.use(cors())
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-});
+socketInit(server)
 
-io.on("connection", async(socket) => {
-  // console.log(`USER CONNECTED: ${socket.id}`);
-  const allSockets = await io.fetchSockets();
-  const allSocketsid = allSockets.map(s=>s.id)
-  console.log('Other sockets : ', allSocketsid)
-  
-  socket.emit("hello", socket.id);
-  io.emit("emitall",allSocketsid)
-
-  socket.on("send_message", (data) => {
-    console.log(`SERVER RECEIVED from`,data);
-    let ID = data.reciverId;
-    if(ID.includes('+room')){
-      console.log("going throung",data.reciverId)
-      io.to(data.reciverId).emit("room_receive_message",data)
-    }
-    else{
-      console.log("-room")
-    socket.to(data.reciverId).emit("receive_message", data);}
-  });
-
-  socket.on("room_req",(data)=>{
-    console.log("request to join room",data)
-    // console.log(socket.id)
-    socket.join(data.roomName)
-    socket.to(data.reciverId).emit("room_invitation",data)
-  })
-
-  socket.on("join_room",(data)=>{
-    socket.join(data)
-  })
-
-  socket.on("disconnect", () => {
-    // console.log(`USER DISCONNECTED: ${socket.id}`);
-    setTimeout(async () => {
-            const remainingSockets = await io.fetchSockets();
-            const remainingUserIds = remainingSockets.map(s => s.id);
-            // Send the reduced list to everyone
-            io.emit("emitall", remainingUserIds);
-        }, 500)
-  });
-});
 
 server.listen(3002, () => console.log("Server is running on port 3002"));
---
+++ server/sockets/operation.js
@@ -0,0 +1,48 @@
+
+module.exports = async (socket, io)=>{
+    // console.log(`USER CONNECTED: ${socket.id}`);
+  const allSockets = await io.fetchSockets();
+  const allSocketsid = allSockets.map(s=>s.id)
+  console.log('Other sockets : ', allSocketsid)
+  
+  socket.emit("hello", socket.id);
+  io.emit("emitall",allSocketsid)
+
+  // Sending message
+  socket.on("send_message", (data) => {
+    console.log(`SERVER RECEIVED from`,data);
+    let ID = data.reciverId;
+    if(ID.includes('+room')){
+      console.log("going throung",data.reciverId)
+      // Which room to recive
+      io.to(data.reciverId).emit("room_receive_message",data)
+    }
+    else{
+      console.log("-room")
+      // which socket to recieve
+    socket.to(data.reciverId).emit("receive_message", data);}
+  });
+
+  // adding user to room
+  socket.on("room_req",(data)=>{
+    console.log("request to join room",data)
+    socket.join(data.roomName)
+    // creator joining a room
+    socket.to(data.reciverId).emit("room_invitation",data)
+  })
+
+  // other user joining a room
+  socket.on("join_room",(data)=>{
+    socket.join(data)
+  })
+
+  socket.on("disconnect", () => {
+    // console.log(`USER DISCONNECTED: ${socket.id}`);
+    setTimeout(async () => {
+            const remainingSockets = await io.fetchSockets();
+            const remainingUserIds = remainingSockets.map(s => s.id);
+            // Send the reduced list to everyone
+            io.emit("emitall", remainingUserIds);
+        }, 500)
+  });
+}
