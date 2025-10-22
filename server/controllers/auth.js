1ec09caa8e2d42bbe66bdcdc2facce321d7346b1
Muntaha <muntahasayed396@gmail.com>
2025-10-23 01:11:47 +0530

    Completed auth in backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET_KEY = 'MY_SECRET_KEY'; 
 
-const login = async (req, res) => {
+const register = async (req, res) => {
     try {
         const { username, email, password, contacts, rooms } = req.body;
 
@@ -51,4 +51,40 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = { login }
+const login = async(req,res)=>{
+
+ try {
+   const { email, password} = req.body;
+ 
+   const UserExist = await User.findOne({email: email})
+ 
+   if(!UserExist){
+     return res.status(400).json({msg: "Please Sign up first"})
+   }
+
+  const isPasswordValid = await bcrypt.compare(password, UserExist.password);;
+ 
+     if (!isPasswordValid) {
+       return res.status(401).json({ msg: "Invalid credentials" });
+     }
+
+     const token = jwt.sign(
+            { 
+                userId: UserExist._id.toString(), 
+                email: UserExist.email 
+            },
+            JWT_SECRET_KEY,
+            { expiresIn: '30d' }
+        );
+ 
+     res.status(200).json({
+       msg: "Login successful",
+       token: token,
+       UserId: UserExist._id.toString(),
+     });
+ } catch (error) {
+  res.status(500).json({ msg: "Internal server error" });
+ }
+}
+
+module.exports = { register, login }
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Auth = require('../controllers/auth')
 
-router.post('/register', Auth.login);
+router.post('/register', Auth.register);
+router.post('/login', Auth.login);
 
 module.exports = router
