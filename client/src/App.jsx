e44974af420cf7af6e285b9b5a96f611ac4b3a59
Muntaha <muntahasayed396@gmail.com>
2025-10-25 02:22:57 +0530

    nothing much small changes in css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -144,8 +144,8 @@ const App = () => {
 
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='h-full flex justify-center bg-gray-900 md:w-[40%] lg:w-[30%]'><UserList/></div>
-      <div className='w-[70%] h-full flex flex-col bg-gray-900 shadow-2xl overflow-hidden'>
+      <div className='h-full flex justify-center bg-gray-900 sm: w-[100%] md:w-[40%] lg:w-[30%]'><UserList/></div>
+      <div className=' sm: w-[0%] md:w-[70%] h-full flex flex-col bg-gray-900 shadow-2xl overflow-hidden'>
         
         <header className='p-4 bg-gray-800 text-white shadow-md'>
           <div className="flex justify-between items-center">
