c33f87116baef8a66ea72edc0ad5da2d1dd85baa
Muntaha <muntahasayed396@gmail.com>
2025-12-10 16:14:54 +0530

    adding dockerfile and configuring client and server for docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true, // Often needed for Docker on Windows/WSL
+    },
+    host: true, // Check this matches the Docker flag
+    strictPort: true,
+    port: 5173, 
+  },
   plugins: [
     tailwindcss(),
     react()],
